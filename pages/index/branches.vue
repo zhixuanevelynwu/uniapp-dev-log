64c5840828f41454d62ced2ac3e2951dd90d7406
evelyn <zw1887@nyu.edu>
2022-07-06 08:55:16 +0800

    added if-statements

---
+++ pages/index/branches.vue
@@ -0,0 +1,38 @@
+<template>
+	<!-- template can only contain 1 direct child element -->
+	<!-- if -->
+	<view>
+		<view v-if="show === 'uni-app'">hello world 0</view>
+		<view>hello world 1</view>
+		<view>{{show ? "show == true" : "show == false"}}</view>
+		
+		<view>
+			<view v-if="show">element 0</view>
+			<!-- <view v-else-if="show">element 0.5</view> -->
+			<view v-else>element 1</view>
+		</view>
+		
+		<button type="default" @click="open">click me</button>
+	</view>
+	
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				show: false
+			}
+		},
+		onLoad() {
+		},
+		methods: {
+			open() {
+				this.show = !this.show
+			}
+		}
+	}
+</script>
+
+<style>
+</style>
