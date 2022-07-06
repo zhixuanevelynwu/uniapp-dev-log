0bf5e59e2dcf4a861bae3697858761f6b89e492c
evelyn <zw1887@nyu.edu>
2022-07-06 08:40:17 +0800

    added basecode and basic syntax

---
+++ pages/index/index.vue
@@ -0,0 +1,38 @@
+<template>
+	<!-- v-bind OR 	: 	represents dynamic class name -->
+	<!-- v-on 	OR	@	represents events -->
+	<view class="content" :class="className" @click="open">
+		{{title}}
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: "hello world :O",
+				className: "active"
+			}
+		},
+		onLoad() {
+			setTimeout(()=>{
+				/*	original way:
+					this.setData({
+						title: "hello uni-app"
+					})
+				*/
+				// this.title = "hello uni-app"
+			}, 2000)
+		},
+		methods: {
+			// custom methods go here
+			open() {
+				console.log("clicked");
+				this.title = "I'm clicked..."
+			}
+		}
+	}
+</script>
+
+<style>
+</style>
